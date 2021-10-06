d08bf28bdb5055bbf0b6d0b25f50d98a4b02e6d7
EbukaMoneme <camoneme@gmail.com>
2021-10-06 21:25:40 +0000

    Implement form validation on tweet form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,49 +6,53 @@
 
 $(() => {
 
+  //Load tweets
+  loadTweets();
+
   //Posts tweets
   const form = $('#tweet-form')
   form.on('submit', function(event) {
     event.preventDefault();
-    console.log('form was submitted');
-    const serliazedData = $(this).serialize();
-    $.post('/tweets', serliazedData)
-      .done(() => {
-        console.log('success');
-        console.log(serliazedData);
+    const serializedData = $(this).serialize();
 
-      }) 
-      .fail(() => {
-        console.log("messed up");
-      }) 
-  })
-  renderTweets(data);
+    if (validateForm(serializedData)) {
+      $.post('/tweets', serializedData)
+        .done(() => {
+          console.log('success');
+          console.log(serializedData);
+          // loadTweets();
+  
+        }) 
+        .fail(() => {
+          console.log("messed up");
+        }) 
+    }
+  })  
 });
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+const validateForm = (tweetData) => {
+  const counter = Number($('.counter').val());
+  if(counter === 140) {
+    window.alert("Tweet field required");
+    return false;
   }
-];
+  if(counter < 0) {
+    window.alert("Exceeding max character count");
+    return false;
+  }
+  return true;
+};
+
+const loadTweets = () => {
+  $.get('/tweets')
+    .done((response) => {
+      console.log(response)
+      renderTweets(response);
+    })
+    .fail((error) => {
+      console.log("Something's wrong:", error);
+    })
+}
 
 const renderTweets = (tweets) => {
   for (const tweet of tweets) {
